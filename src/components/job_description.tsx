912187f96bc72926f4d74f5e58992a7308844199
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-04-27 12:32:39 +0100

    Initial commit

--- src/components/job_description.tsx
+++ src/components/job_description.tsx
@@ -14,6 +14,7 @@ export function JobDescription(
     return (
         <>
             <Grid container justifyContent={"center"} sx={{margin: "2rem"}}>
+
                 <Grid item xs={12} md={12} lg={12}>
                     <Typography variant={'h6'} textAlign={"center"}>
                         {company_name}
